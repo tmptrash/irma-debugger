af23ab1ab1610b614208b1c185ac57bf9e250e3c
flatline <deadbrainman@gmail.com>
2019-12-19 19:02:47 +0200

    app skeleton #1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Config from './../config/Config';
 
 function App() {
   return (
     <div className="App">
       <h1>irma debugger</h1>
+      <Config></Config>
     </div>
   );
 }
---
+++ src/components/config/Config.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Config() {
+  return (
+    <div className="Config">
+      <textarea></textarea>
+    </div>
+  );
+}
+
+export default Config;
