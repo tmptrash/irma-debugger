2e39326c57e8b8dcd34aed5dab947c61a1e5f092
flatline <deadbrainman@gmail.com>
2019-12-26 17:20:37 +0200

    added items to Info component #1

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Info.scss';
 import BioVM from './../../BioVM'; 
 import Store from './../../Store';
+import Mutations from 'irma/src/irma/Mutations';
 
 class Info extends React.Component {
   constructor() {
@@ -26,15 +27,17 @@ class Info extends React.Component {
   componentWillUnmount() {this._unsubscribe()}
 
   render () {
-    if (!this._debug) {
-      return (<div className="info">Press "Step" to start debug</div>)
-    }
+    if (!this._debug) {return (<div className="info">Press "Step" to start debug</div>)}
 
     const org = BioVM.getVM().orgs.get(0);
     return (
       <div className="info">
-        <div className="regs">registers:</div>
-        <div>ax: {org.ax} bx: {org.bx}</div>
+        <div className="regs">ax: {org.ax} bx: {org.bx} ret: {org.ret}</div>
+        <div className="mem">{Array.from(org.mem).map((m, i) => <span key={i} className={org.memPos === i ? 'active' : ''}>{m} </span>)}</div>
+        <div className="age">age: {org.age}</div>
+        <div className="probs">{Array.from(org.probs).map((p, i) => <span key={i}>{Mutations.NAMES[i]}: {p} </span>)}</div>
+        <div className="period">period: {org.period}</div>
+        <div className="percent">percent: {org.percent}</div>
       </div>
     );
   }
