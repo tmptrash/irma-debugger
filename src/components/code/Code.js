c606f452f64e3157a2da03ed18f4922b32bef02b
DeadbraiN <deadbrainman@gmail.com>
2020-01-18 23:47:27 +0200

    styling #1

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -118,7 +118,7 @@ class Code extends React.Component {
                 {/* <textarea title={errMsg} className={validCls} value={value} onChange={onChange} onScroll={onScroll}></textarea> */}
                 <ControlledEditor
                     width="400px"
-                    height="600px"
+                    height="668px"
                     language="line"
                     theme="lineTheme"
                     value={value}
