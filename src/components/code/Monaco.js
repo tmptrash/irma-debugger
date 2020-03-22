52f9d02c4745d063c612c6971ca9e07739cc5230
flatline <deadbrainman@gmail.com>
2020-03-22 21:42:06 +0200

    updated debugger according to new irma VM #1

--- src/components/code/Monaco.js
+++ src/components/code/Monaco.js
@@ -11,13 +11,14 @@ class Monaco {
     static init(monaco) {
         monaco.languages.register({ id: 'line' });
         monaco.languages.setMonarchTokensProvider('line', {
-            keywordsCore : ['toggle','eq','nop','add','sub','mul','div','inc','dec','rshift','lshift','rand','call','ret','nand','age','line','len','left','right','save','load','savea','loada','read','break'],
+            keywordsCore : ['toggle','eq','nop','add','sub','mul','div','inc','dec','rshift','lshift','rand','nand','age','line','len','left','right','save','load','savea','loada','read'],
             keywordsBlock: ['func', 'loop', 'ifg', 'ifl', 'ife', 'ifne', 'ifp', 'ifn', 'ifz', 'end'],
+            keywordsFlow : ['ret','break','cont','call'],
             keywordsBio  : ['join','split','step','see','say','listen','nread','get','put','offs','color','anab','catab','mol','mmol','smol','rmol','lmol','cmol','mcmp','find','reax','dir','lhead','rhead'],
             tokenizer: {
                 root: [
                     [/\s*\d+/, "line-num"],
-                    [/[a-z0-9]+/, {cases: {'@keywordsCore': "line-core", '@keywordsBio': "line-bio", '@keywordsBlock': "line-block"}}],
+                    [/[a-z0-9]+/, {cases: {'@keywordsCore': "line-core", '@keywordsBio': "line-bio", '@keywordsBlock': "line-block", '@keywordsFlow': "line-flow"}}],
                     [/\s*#.*/, "line-comment"],
                     [/@\s*[a-zA-Z_\$][\w\$]*/, 'line-ann']
                 ]
@@ -29,6 +30,7 @@ class Monaco {
             rules: [
                 { token: 'line-core',    foreground: '005c80' },
                 { token: 'line-block',   foreground: 'ff8040' },
+                { token: 'line-flow',    foreground: 'ffa274' },
                 { token: 'line-num',     foreground: '008040', fontStyle: 'bold' },
                 { token: 'line-comment', foreground: '555555' },
                 { token: 'line-bio',     foreground: '005c00' },
